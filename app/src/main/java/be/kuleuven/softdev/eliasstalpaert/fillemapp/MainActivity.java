8cf74e2a39ea74485427c87728659eb76be47db4
elias.stalpaert <elias.stalpaert@student.kuleuven.be>
2018-05-16 13:39:09 +0200

    Versie voor navigation drawer

--- app/src/main/java/be/kuleuven/softdev/eliasstalpaert/fillemapp/MainActivity.java
+++ app/src/main/java/be/kuleuven/softdev/eliasstalpaert/fillemapp/MainActivity.java
@@ -13,34 +13,59 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.edmodo.rangebar.RangeBar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private RatingBar ratingBar;
+
     private TextView textView_movie;
     private TextView textView_rating;
+    private TextView textView_beginyear;
+    private TextView textView_endyear;
+
     private Button button_movie;
+    private ArrayList<Integer> genres_true;
+    private HashMap<Integer,String> genres;
     private RequestQueue requestQueue;
+
     private JSONObject movie;
+
     private String current_movie_id;
+
+    private RangeBar rangeBar;
+
+    private Integer beginyear;
+    private Integer endyear;
+    private Float rating_float;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //initialize variables
+        genres_true = new ArrayList<>();
         requestQueue = Volley.newRequestQueue(getApplicationContext());
         ratingBar = findViewById(R.id.ratingBar);
         textView_movie = findViewById(R.id.textView_movie);
         textView_rating = findViewById(R.id.textView_rating);
+        textView_beginyear = findViewById(R.id.textView_beginyear);
+        textView_endyear = findViewById(R.id.textView_endyear);
         button_movie = findViewById(R.id.button_movie);
+        rangeBar = findViewById(R.id.rangebar);
         //init routine
-        textView_rating.setText("Rating");
-        textView_movie.setText("Movie ID");
+        init();
 
+//        textView_beginyear.setText(rangeBar.getLeftIndex() + 1894);
+//        textView_endyear.setText(rangeBar.getRightIndex() + 1894);
         //Set on click listeners
         button_movie.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,10 +76,34 @@ public class MainActivity extends AppCompatActivity {
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Float r = rating*2;
-                textView_rating.setText(r.toString());
+                rating_float = rating * 2;
+                textView_rating.setText(rating_float.toString());
             }
         });
+        rangeBar.setOnRangeBarChangeListener(new RangeBar.OnRangeBarChangeListener() {
+            @Override
+            public void onIndexChangeListener(RangeBar rangeBar, int i, int i1) {
+                beginyear = i+1894;
+                endyear = i1+1894;
+                if(beginyear < 1894){
+                    beginyear = 1894;
+                }
+                if(endyear > 2018){
+                    endyear = 2018;
+                }
+                String s1 = beginyear.toString();
+                String s2 = endyear.toString();
+                textView_beginyear.setText(s1);
+                textView_endyear.setText(s2);
+            }
+        });
+    }
+
+    public void init() {
+        rangeBar.setTickCount(127);
+        rangeBar.setThumbRadius(10);
+        textView_rating.setText("Rating");
+        textView_movie.setText("Movie ID");
     }
 
     public void generateMovie() {
