c3305c8d7bb9ee2f0f3325527f6bb95503cac5f9
BuildTools <unconfigured@null.spigotmc.org>
2018-05-28 02:05:36 +0200

    More colors!

--- app/src/main/java/be/kuleuven/softdev/eliasstalpaert/fillemapp/MainActivity.java
+++ app/src/main/java/be/kuleuven/softdev/eliasstalpaert/fillemapp/MainActivity.java
@@ -96,6 +96,23 @@ public class MainActivity extends AppCompatActivity {
         this.initFilters();
         this.initMovieGen();
 
+        this.initColours();
+
+    }
+
+    private void initColours(){
+        rangeBarYear.setConnectingLineColor(getIntFromColor(128,128,128));
+        rangeBarYear.setBarColor(getIntFromColor(0,0,0));
+        rangeBarYear.setThumbColorNormal(getIntFromColor(192,192,192));
+        rangeBarYear.setThumbColorPressed(getIntFromColor(169,169,169));
+    }
+
+    public int getIntFromColor(int Red, int Green, int Blue){
+        Red = (Red << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
+        Green = (Green << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
+        Blue = Blue & 0x000000FF; //Mask out anything not blue.
+
+        return 0xFF000000 | Red | Green | Blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
     }
 
     public static void populateWatchString(){
